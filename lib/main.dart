c62f1690b608c122c2fc680c4ab401b5b253dafd
Alessio Caputo <84250128+alessioC42@users.noreply.github.com>
2025-06-25 18:27:37 +0200

    feat: Add dark theme support

--- lib/main.dart
+++ lib/main.dart
@@ -25,8 +25,19 @@ class App extends StatelessWidget {
         useMaterial3: true,
         sliderTheme: const SliderThemeData(
           year2023: false,
-        )
+        ),
       ),
+      darkTheme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.blue[900]!,
+          brightness: Brightness.dark,
+        ),
+        useMaterial3: true,
+        sliderTheme: const SliderThemeData(
+          year2023: false,
+        ),
+      ),
+      themeMode: ThemeMode.system,
       home: Scaffold(
         body: StreamBuilder(
           stream: pb.authStore.onChange,
