0ee5acaf101deebae0f4360ff77ed2ec4cb044a7
Alessio Caputo <84250128+alessioC42@users.noreply.github.com>
2025-07-17 18:40:46 +0200

    feat: implement automatic login flow and add entitlements for associated domains

--- lib/main.dart
+++ lib/main.dart
@@ -1,40 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:pocketbase/pocketbase.dart';
 import 'package:zeyn/api/pocketbase.dart';
 import 'package:zeyn/views/admin/admin_home_view.dart';
+import 'package:zeyn/views/login_route_screen.dart';
 import 'package:zeyn/views/login_screen.dart';
 import 'package:zeyn/views/students/student_home_view.dart';
 import 'package:zeyn/views/teachers/teacher_home_view.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await initPocketbase();
-  runApp(const App());
-}
-
-class App extends StatelessWidget {
-  const App({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Zeyn',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue[900]!),
-        useMaterial3: true,
-        sliderTheme: const SliderThemeData(year2023: false),
-      ),
-      darkTheme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: Colors.blue[900]!,
-          brightness: Brightness.dark,
-        ),
-        useMaterial3: true,
-        sliderTheme: const SliderThemeData(year2023: false),
-      ),
-      themeMode: ThemeMode.system,
-      home: Scaffold(
+final router = GoRouter(
+  routes: [
+    GoRoute(path: '/', builder: (context, state) => Scaffold(
         body: StreamBuilder(
           stream: pb.authStore.onChange,
           builder: (
@@ -54,6 +30,40 @@ class App extends StatelessWidget {
           },
         ),
       ),
+    ),
+    GoRoute(path: ('/login'), builder: (context, state) => LoginRouteScreen(queryParams: state.uri.queryParameters)),
+  ],
+);
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await initPocketbase();
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      title: 'Zeyn',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue[900]!),
+        useMaterial3: true,
+        sliderTheme: const SliderThemeData(year2023: false),
+      ),
+      darkTheme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.blue[900]!,
+          brightness: Brightness.dark,
+        ),
+        useMaterial3: true,
+        sliderTheme: const SliderThemeData(year2023: false),
+      ),
+      themeMode: ThemeMode.system,
+      routerConfig: router,
     );
   }
 }
---
+++ lib/views/login_route_screen.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:zeyn/api/pocketbase.dart';
+import 'package:zeyn/components/student_qr_login_screen.dart';
+import 'package:zeyn/utils/dialogs/show_confirm_dialog.dart';
+import 'package:zeyn/utils/dialogs/show_error_dialog.dart';
+import 'package:zeyn/utils/dialogs/show_loading_dialog.dart';
+import 'package:zeyn/utils/logger.dart';
+
+class LoginRouteScreen extends StatefulWidget {
+  final Map<String, String> queryParams;
+  const LoginRouteScreen({super.key, required this.queryParams});
+
+  @override
+  State<LoginRouteScreen> createState() => _LoginRouteScreenState();
+}
+
+class _LoginRouteScreenState extends State<LoginRouteScreen> {
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      handleLogin();
+    });
+  }
+
+  void handleLogin() async {
+    if (pb.authStore.isValid) {
+      final result = await showConfirmDialog(context, message: "Möchtest du dich wirklich abmelden? Du bist bereits angemeldet.");
+      if (result == false && mounted) {
+        context.go('/');
+        return;
+      }
+    }
+
+    try {
+      if (widget.queryParams["token"] == null && mounted) {
+        context.go('/');
+        return;
+      }
+      showLoadingDialog(context, message: "Automatische Anmeldung...");
+      final loginAuthRecord = StudentQrModel.fromLoginToken(widget.queryParams["token"]!);
+      await pb
+          .collection('students')
+          .authWithPassword(
+        loginAuthRecord.email,
+        loginAuthRecord.password,
+      );
+      if (!mounted) return;
+      Navigator.of(context).pop();
+      if (!mounted) return;
+      context.go('/');
+    } catch (e, s) {
+      logger.e(e, stackTrace: s);
+      if (!mounted) return;
+      Navigator.of(context).pop();
+      if (!mounted) return;
+      await showErrorDialog(context, message: "Automatische Anmeldung fehlgeschlagen. Bitte versuche es erneut.");
+      if (!mounted) return;
+      context.go('/');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Automatische Anmeldung'),
+      ),
+      body: Center(
+        child: CircularProgressIndicator(),
+      ),
+    );
+  }
+}
